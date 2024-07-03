c48d6d70731efd72c9713db461a2e6ae4294570c
Mohammed Shaban <110273856+Millo9@users.noreply.github.com>
2024-07-03 20:30:43 +0300

    index.html

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<?php
-  if(isset($_POST['button'])){
-    $imgUrl = $_POST['imgurl'];
-    $ch = curl_init($imgUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $downloadImg = curl_exec($ch);
-    curl_close($ch);
-    header('Content-type: image/jpg');
-    header('Content-Disposition: attachment;filename="thumbnail.jpg"');
-    echo $downloadImg;
-  }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,7 +8,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 </head>
 <body>
-  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+  <form action="download.php" method="POST">
     <header>Download Thumbnail</header>
     <div class="url-input">
       <span class="title">Paste video url:</span>
